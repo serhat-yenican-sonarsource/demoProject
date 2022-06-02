46d404bed6ec6779c88edf56ecd0b3c0884504e6
serhat-yenican-sonarsource <serhat.yenican@sonarsource.com>
2022-06-02 21:00:51 +0200

    initial commit

---
+++ demo/src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+    private static final String MESSAGE = "Hello";
+
+    public String hello(String name) {
+        return MESSAGE + name;
+    }
+
+}
+
