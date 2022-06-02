a6d85277d16403211381c74e66015ce7887f434d
serhat-yenican-sonarsource <serhat.yenican@sonarsource.com>
2022-06-02 21:22:44 +0200

    new problems

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,8 @@ import java.net.PasswordAuthentication;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.StringTokenizer;
+
 
 public class App {
     private static final String MESSAGE = "Hello";
@@ -40,6 +42,18 @@ public class App {
         String uname = "steve";
         String password = "blue";
         conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+          "user=" + uname + "&password=" + password); // Sensitive
+
+        return new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
+    }
+
+    public PasswordAuthentication hotSpotExample2() throws SQLException {
+        Connection conn2 = null;
+        conn2 = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+          "user=steve&password=blue"); // Sensitive
+        String uname = "steve";
+        String password = "blue";
+        conn2 = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
           "user=" + uname + "&password=" + password); // Sensitive
 
         return new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
