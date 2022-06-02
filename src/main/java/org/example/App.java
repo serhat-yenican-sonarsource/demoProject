326cd597e4eed8af9e5bda6895d8ec4ee7e23305
serhat-yenican-sonarsource <serhat.yenican@sonarsource.com>
2022-06-02 21:45:33 +0200

    all types

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,37 @@
 package org.example;
 
-public class App {
-    private static final String MESSAGE = "Hello";
 
-    public String hello(String name) {
-        return MESSAGE + name;
+import java.net.PasswordAuthentication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.StringTokenizer; //code smells
+
+
+public class App {
+
+    public void bug() {
+        Thread thread = new Thread();
+        thread.run();
     }
+
+    @Override
+    protected Object clone() {
+        return null; //vulnerability
+    }
+
+    public PasswordAuthentication hotSpotExample() throws SQLException {
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+          "user=steve&password=blue"); // Sensitive
+        String uname = "steve";
+        String password = "blue";
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+          "user=" + uname + "&password=" + password); // Sensitive
+
+        return new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
+    }
+
 
 }
 
