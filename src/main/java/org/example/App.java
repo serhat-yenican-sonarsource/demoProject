f4a12e5bc6dee9ab42b10290f0084948f65e4638
Serhat Yenican <serhat.yenican@sonarsource.com>
2022-06-21 14:11:21 +0200

    all good

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,47 +1,14 @@
 package org.example;
 
-
-import java.net.PasswordAuthentication;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Map;
-
-
 public class App {
+    private static final String MESSAGE = "Hello";
 
-    public void bug() {
-        Thread thread = new Thread();
-        thread.run();
+    public String hello(String name) {
+        return MESSAGE + name;
     }
 
-    @Override
-    protected Object clone() {
-        return null;
+    public String getMessage() {
+        return MESSAGE;
     }
-
-    public Connection vulnerability(Map<String, String> request) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "login", "");
-        return conn;
-    }
-
-    public Connection vulnerability2(Map<String, String> request) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "login", "");
-        return conn;
-    }
-
-    public PasswordAuthentication hotSpotExample() throws SQLException {
-        Connection conn = null;
-        conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-          "user=steve&password=blue"); // Sensitive
-        String uname = "steve";
-        String password = "blue";
-        conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-          "user=" + uname + "&password=" + password); // Sensitive
-
-        return new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
-    }
-
-
 }
 
