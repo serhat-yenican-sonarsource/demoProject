7da2bd664ce43e634e16780fc566a60c8bec73b8
Serhat Yenican <serhat.yenican@sonarsource.com>
2022-09-05 22:52:06 +0200

    all good 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class App {
     private static final String MESSAGE = "Hello";
-    private static final String MESSAGE2 = "Hello2";
-    private static final String MESSAGE3 = "Hello3";
 
     public String hello(String name) {
         return MESSAGE + name;
