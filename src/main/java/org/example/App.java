743c414bb08f1b2359b7424295f39bd3f38bcc4a
Serhat Yenican <serhat.yenican@sonarsource.com>
2022-09-16 11:20:16 +0200

    1 major code smell added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,9 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
+
+    public static void main(String[] args) {
+        System.out.println(new App().hello(null));
+    }
 }
 
