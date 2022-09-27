fb4fde28d7869476e6a9eb5c5a8a05466661f2aa
Serhat Yenican <serhat.yenican@sonarsource.com>
2022-09-27 17:32:39 +0200

    1 major code smell added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,7 @@ package org.example;
 public class App {
     private static final String MESSAGE = "Hello";
     private static final String MESSAGE2 = "Hello";
-    private static final String MESSAGE2 = "Hello";
+    private static final String MESSAGE3 = "Hello";
 
     public String hello(String name) {
         return MESSAGE + name;
