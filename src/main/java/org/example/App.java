f17f680e2ca619687a00823a02fc5c3b76239249
Serhat Yenican <serhat.yenican@sonarsource.com>
2022-09-29 08:57:02 +0200

    1 major code smell added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class App {
     private static final String MESSAGE = "Hello";
     private static final String MESSAGE2 = "Hello";
@@ -11,5 +15,21 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Path tempDir = Files.createTempDirectory("clone");
+        System.out.println(tempDir);
+        ProcessBuilder processBuilder = new ProcessBuilder("ls", "/var");
+        processBuilder.environment().put("srt", "/temp");
+        Process process = processBuilder
+          .redirectErrorStream(true)
+          .start();
+        process.getInputStream().transferTo(System.out);
+        int exitCode = process.waitFor();
+
+        Files.delete(tempDir);
+    }
+
+
 }
 
