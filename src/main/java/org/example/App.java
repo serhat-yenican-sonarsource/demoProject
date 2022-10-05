acf66f6a4bfec7a68cc24cceac6d8f985449c5b5
Serhat Yenican <serhat.yenican@sonarsource.com>
2022-10-05 12:45:37 +0200

    1 major code smell added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 public class App {
     private static final String MESSAGE = "Hello";
     private static final String MESSAGE2 = "Hello";
-    private static final String MESSAGE2 = "Hello";
 
     public String hello(String name) {
         return MESSAGE + name;
