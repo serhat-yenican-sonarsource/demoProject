f1a96f9446f57201352298240147a170e0f00018
Serhat Yenican <serhat.yenican@sonarsource.com>
2023-01-02 13:43:36 +0100

    PR commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,10 +3,6 @@ package org.example;
 public class App {
     private static final String MESSAGE = "Hello";
 
-    public String hello(String name) {
-        return MESSAGE + name;
-    }
-
     public String getMessage() {
         return MESSAGE;
     }
