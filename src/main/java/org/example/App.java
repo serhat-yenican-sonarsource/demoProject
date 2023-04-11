8edfa783e2616ed4f913ee79b4571c01198f35c1
Serhat Yenican <serhat.yenican@sonarsource.com>
2023-04-11 18:07:47 +0200

    configurations

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,11 @@ package org.example;
 
 public class App {
     private static final String MESSAGE = "Hello";
+    private static final String MESSAGE2 = "Hello";
 
     public String getMessage() {
         return MESSAGE;
     }
+
 }
 
