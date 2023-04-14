0a6703f9b394752c2b4ab66f6a51b0a59f72c775
serhat yenican <serhatyenican@gmail.com>
2023-04-14 19:09:16 +0200

    Update App.java
    stupid commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,23 @@ package org.example;
 
 public class App {
     private static final String MESSAGE = "Hello";
+    private static final String MESSAGE2 = "Hello";
+    private static final String MESSAGE3 = "Hello";
+    private static final String MESSAGE4 = "Hello";
+    private static final String MESSAGE5 = "Hello";
+    private static final String MESSAGE6 = "Hello";
+    private static final String MESSAGE7 = "Hello";
+    private static final String MESSAGE8 = "Hello";
 
     public String hello(String name) {
         return MESSAGE + name;
     }
 
     public String getMessage() {
+        return MESSAGE;
+    }
+
+    public String getMessages() {
         return MESSAGE;
     }
 }
