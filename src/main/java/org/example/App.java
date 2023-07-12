af4e6944382488cc975e84bfaef7c5fc7ef38f58
Serhat Yenican <serhat.yenican@sonarsource.com>
2023-07-12 10:14:15 +0200

    new line

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ package org.example;
 public class App {
     private static final String MESSAGE = "Hello";
     private static final String MESSAGE = "Hello";
+    private static final String MESSAGE = "Hello";
 
     public String hello(String name) {
         return MESSAGE + name;
