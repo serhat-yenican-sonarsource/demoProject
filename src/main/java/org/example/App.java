43417c8b5561217d1593ba464b663fb4d04a193d
Serhat Yenican <serhat.yenican@sonarsource.com>
2023-09-29 22:45:13 +0200

    change in main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,6 @@ package org.example;
 public class App {
     private static final String MESSAGE = "Hello";
     private static final String MESSAGE = "Hello";
-    private static final String MESSAGE = "Hello";
 
     public String hello(String name) {
         return MESSAGE + name;
