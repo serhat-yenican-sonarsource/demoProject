bad6ec32ed00ab1e617ccc3afde2564c91576604
Serhat Yenican <serhat.yenican@sonarsource.com>
2024-03-12 14:31:12 +0100

    srt

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,13 +3,43 @@ package org.example;
 public class App {
     private static final String MESSAGE = "Hello";
     private static final String MESSAGE1 = "Hello";
+    private static final String secret = "my-secret";
+    private static final String PASSWORD = "my-password";
+    private static final String PASSWORD2 = "my-password";
+    private static final String PASSWORD3 = "my-password";
+    private static final String PASSWORD4 = "my-password";
+    private static final String PASSWORD5 = "my-password";
 
     public String hello(String name) {
         return MESSAGE + name;
     }
 
-    public String getMessage() {
+    public static String getMessage() {
         return MESSAGE;
+    }
+
+    public static String getMessage2() {
+        return "";
+    }
+
+    public static String getPswd() {
+        return "" + "asd" + "pswd";
+    }
+
+    public boolean isValid(String password) {
+        return PASSWORD.equals(password);
+    }
+
+    private static final String MY_SECRET = "47828a8dd77ee1eb9dde2d5e93cb221ce8c32b37";
+
+    public static void main(String[] args) {
+        new App().callMyService(MY_SECRET);
+    }
+
+
+
+    private void callMyService(String mySecret) {
+        System.out.println(isValid(mySecret));
     }
 }
 
