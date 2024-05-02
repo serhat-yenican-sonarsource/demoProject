141c100930e39438bafcd5300c23a41ac92785dd
Serhat Yenican <serhat.yenican@sonarsource.com>
2024-05-02 12:08:42 +0200

    check 4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,6 +16,7 @@ public class App {
     private static final String PASSWORD5 = "my-password";
     private static final String PASSWORD6 = "my-password";
     private static final String PASSWORD7 = "my-password";
+    private static final String PASSWORD8 = "my-password";
 
     public String hello(String name) {
         return MESSAGE + name;
