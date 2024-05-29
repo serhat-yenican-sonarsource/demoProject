90a83ba46b747d435b24925f7f73d3732c24effc
Serhat Yenican <serhat.yenican@sonarsource.com>
2024-05-29 14:47:43 +0200

    some issues

--- src/main/java/Root.java
+++ src/main/java/Root.java
@@ -1,2 +1,13 @@
 public class Root {
+  private static final String MESSAGE = "Hello";
+  private String fileName;
+
+  public Root(String fileName) {
+    //TODO check
+    this.fileName = fileName;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
 }
